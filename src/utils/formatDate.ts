e16b93e6d70cdb5692d31b192b1745b597d04f12
zkp <2470904132@qq.com>
2025-01-02 20:18:36 +0800

    feat: 改进类型安全并优化配置
    为blog分页组件添加类型定义
    更新了 formatDate.ts 文件，改进了日期处理逻辑，特别是对 Date 对象的处理
    调整tsconfig.json，放宽严格类型检查规则

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,18 +1,20 @@
 import dayjs from "dayjs";
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import utc from "dayjs/plugin/utc";
-import {t} from '../i18n/utils';
-import {config} from '../consts';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import { config } from '../consts';
+import { t } from '../i18n/utils';
 
 dayjs.locale(config.lang);
-dayjs.extend(advancedFormat)
-dayjs.extend(utc);
+dayjs.extend(advancedFormat);
 
-export function formatDate(date,dateType='post.dateFormat') {
-  if (date) {
-    const dateFormat = t(dateType) || "YYYY-MM-DD HH:mm:ss";
-    return dayjs(date).utc().format(dateFormat);
-  } else {
-    return ''
-  }
+export function formatDate(date, dateType = 'post.dateFormat') {
+    if (date) {
+        const dateFormat = t(dateType) || "YYYY-MM-DD HH:mm:ss";
+        // 对于 Date 对象（来自 fs.statSync），直接使用 toISOString 方法去掉时区信息
+        if (date instanceof Date) {
+            return dayjs(date.toISOString().split('Z')[0]).format(dateFormat);
+        }
+        // 对于字符串日期，直接解析
+        return dayjs(date).format(dateFormat);
+    }
+    return '';
 }
