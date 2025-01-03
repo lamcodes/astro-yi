724dc7cde5bd743860fa23d944b8ae90fd4d78fe
zkp <2470904132@qq.com>
2025-01-03 18:10:50 +0800

    fix：修改日期的展示

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -1,14 +1,14 @@
 import { statSync } from "fs";
 import { toString } from 'mdast-util-to-string';
 import getReadingTime from 'reading-time';
-import { formatDate } from "../utils/formatDate.ts";
+import { formatDateUtc } from "../utils/formatDate.ts";
 
 export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0];
         const stats = statSync(filepath);
-        // 直接传入 Date 对象
-        file.data.astro.frontmatter.lastModified = formatDate(stats.mtime);
+        // 直接传入 Date 对象 stats.mtime.toISOString() '2025-01-02T12:03:08.316Z' 实际是20.03，但是显示为utc。
+        file.data.astro.frontmatter.lastModified = formatDateUtc(stats.mtime);
 
         // 获取文章字数和阅读时长
         const textOnPage = toString(tree);
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc.js';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import { config } from '../consts';
 import { t } from '../i18n/utils';
 
 dayjs.locale(config.lang);
+dayjs.extend(utc);
 dayjs.extend(advancedFormat);
 
 export function formatDate(date, dateType = 'post.dateFormat') {
@@ -14,6 +16,15 @@ export function formatDate(date, dateType = 'post.dateFormat') {
             return dayjs(date.toISOString().split('Z')[0]).format(dateFormat);
         }
         // 对于字符串日期，直接解析
+        return dayjs(date).format(dateFormat);
+    }
+    return '';
+}
+
+export function formatDateUtc(date, dateType = 'post.dateFormat') {
+    if (date) {
+        const dateFormat = t(dateType) || "YYYY-MM-DD HH:mm:ss";
+        // 对于 Date 对象（来自 fs.statSync），转换为本地时间并格式化
         return dayjs(date).format(dateFormat);
     }
     return '';
